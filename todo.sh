f5211f39b750e59008e5976a2cdaac7e50cc36fd
Roger Almeida <ralmeida@tyro.com>
2017-02-24 17:00:25 +1100

    Listing, adding and completing tasks \o/

--- todo.sh
+++ todo.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 declare -a TODO_ITEMS
+declare -a DONE_ITEMS
+
 IFS=""
 function readTodoFile {
-    local COUNT=1
     while read -r LINE; do
-      if [ "$LINE" != "[TODO]" ]
-      then
-        TODO_ITEMS[${COUNT}]="$LINE"
-        let "COUNT++"
-      fi
+      TODO_ITEMS+=("$LINE")
     done < .todo.txt
 }
 
-function appendCurrentToArray {
+function readDoneFile {
+    while read -r LINE; do
+      DONE_ITEMS+=("$LINE")
+    done < .done.txt
+}
+
+function appendCurrentToToDoArray {
   local SIZE=${#TODO_ITEMS[@]}
   let "SIZE++"
   local ITEM=""
@@ -23,62 +26,92 @@ function appendCurrentToArray {
   TODO_ITEMS[${SIZE}]="${ITEM}"
 }
 
+function appendCurrentToDoneArray {
+  DONE_ITEMS+=(TODO_ITEMS[$1])
+}
+
 function writeBackToFile {
   > .todo.txt
-  local COUNT=1
-  echo "[TODO]" >> .todo.txt
-  echo "TODO:"
   for ITEM in ${TODO_ITEMS[@]}; do
     echo "${ITEM}" >> .todo.txt
-    echo "${COUNT}${ITEM}"
-    let "COUNT++"
+  done
+}
+
+function writeBackToDoneFile {
+  > .done.txt
+  for ITEM in ${DONE_ITEMS[@]}; do
+    echo "${ITEM}" >> .done.txt
   done
 }
 
 function add {
-  if [ -e .todo.txt ]
-  then
-    readTodoFile
-  fi
-  appendCurrentToArray $@
+  readTodoFile
+  appendCurrentToToDoArray $@
   writeBackToFile
+  justListTodos
 }
 
 function printHelp {
   cat << EndOfMessage
     Usage:
       todo -a|--add <task>
+      todo -d|--done <task's index to mark as completed>
 EndOfMessage
 }
 
 function completeTask {
-  if [ -e .todo.txt ]
-  then
-    readTodoFile
-  fi
-  unset TODO_ITEMS[$1]
+  readTodoFile
+  readDoneFile
+
+  appendCurrentToDoneArray $@
+
+  local INDEX=$1
+  INDEX=$((INDEX-1))
+  unset TODO_ITEMS[$INDEX]
   writeBackToFile
+  writeBackToDoneFile
+  justListTodos
 }
 
-while [[ $# -gt 1 ]]
+function justListTodos {
+  local COUNT=1
+  echo "TODO:"
+  for ITEM in ${TODO_ITEMS[@]}; do
+    echo "${COUNT}${ITEM}"
+    let "COUNT++"
+  done
+}
+
+function listTodos {
+  readTodoFile
+  justListTodos
+}
+
+while [[ $# -ge 1 ]]
 do
   key="$1"
 
   case $key in
       -a|--add)
-      shift # past argument
-      add $@
-      exit 0
+        shift # past argument
+        add $@
+        exit 0
       ;;
       -d|--done)
-      shift # past argument
-      completeTask $@
+        shift # past argument
+        completeTask $@
+        exit 0
+      ;;
+      -l|--list)
+        shift
+        listTodos
+        exit 0
       ;;
       *)
+        shift
         printHelp        # unknown option
+        exit 0
       ;;
   esac
   shift # past argument or value
 done
-
-
